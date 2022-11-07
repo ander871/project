2ecf56a87fb8204632ff08349eb1a41391f2297b
Ivan <example@gmail.com>
2022-11-07 07:00:30 -0500

    Practise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('hi');
+// console.log("hi");
+const numberOfFilms = +prompt("skolko filmov posmotrel", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let oneOfLastFilms = prompt("oneOfLastFilms", "");
+let ozenka10 = prompt("ozenka ot 0 do 10", "");
+personalMovieDB.movies[oneOfLastFilms] = ozenka10;
+
+oneOfLastFilms = prompt("oneOfLastFilms", "");
+ozenka10 = prompt("ozenka ot 0 do 10", "");
+personalMovieDB.movies[oneOfLastFilms] = ozenka10;
+
+console.log(personalMovieDB);
